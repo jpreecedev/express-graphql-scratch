3a1c5400fd9d62aed71b34d88126ea445dbbf7fe
Jon Preece <jonpreece@hotmail.co.uk>
2018-07-28 06:02:59 +0100

    Configure client side apollo graphql

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,18 +1,12 @@
 import React, { Component } from "react"
+import gql from "graphql-tag"
+import { Query } from "react-apollo"
+
 import styles from "./styles.scss"
 
 class App extends Component {
   state = {
     message: "Hello, World!"
-  }
-
-  componentDidMount = () => {
-    fetch("http://localhost:3002/graphql")
-      .then(response => response.json())
-      .then(response => {
-        console.log(response)
-        debugger
-      })
   }
 
   showMessage = () => {
@@ -21,13 +15,23 @@ class App extends Component {
 
   render() {
     return (
-      <header>
-        <h1>Hello, World!</h1>
-        <h2 className={styles.module}>Goodbye</h2>
-        <button onClick={this.showMessage}>Click Me</button>
-      </header>
+      <Query query={SAY_HELLO} variables={{ name: "Jon" }}>
+        {({ data }) => (
+          <header>
+            <h1>{data.hello}</h1>
+            <h2 className={styles.module}>Goodbye</h2>
+            <button onClick={this.showMessage}>Click Me</button>
+          </header>
+        )}
+      </Query>
     )
   }
 }
+
+const SAY_HELLO = gql`
+  query sayHello($name: String!) {
+    hello(name: $name)
+  }
+`
 
 export default App
