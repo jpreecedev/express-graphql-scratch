3d616cdec3aaf3ee932c3243c3f5660633ea9242
Jon Preece <jonpreece@hotmail.co.uk>
2018-07-28 06:08:14 +0100

    Tidy up

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,32 +1,18 @@
-import React, { Component } from "react"
+import React from "react"
 import gql from "graphql-tag"
 import { Query } from "react-apollo"
 
 import styles from "./styles.scss"
 
-class App extends Component {
-  state = {
-    message: "Hello, World!"
-  }
-
-  showMessage = () => {
-    alert(this.state.message)
-  }
-
-  render() {
-    return (
-      <Query query={SAY_HELLO} variables={{ name: "Jon" }}>
-        {({ data }) => (
-          <header>
-            <h1>{data.hello}</h1>
-            <h2 className={styles.module}>Goodbye</h2>
-            <button onClick={this.showMessage}>Click Me</button>
-          </header>
-        )}
-      </Query>
-    )
-  }
-}
+const App = () => (
+  <Query query={SAY_HELLO} variables={{ name: "Jon" }}>
+    {({ data }) => (
+      <header>
+        <h1>{data.hello}</h1>
+      </header>
+    )}
+  </Query>
+)
 
 const SAY_HELLO = gql`
   query sayHello($name: String!) {
